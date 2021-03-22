f9c9900e0a3b83daf3aa08fbe13024bc66993f1d
Christin <urbanelation@gmx.de>
2021-03-22 18:00:43 +0100

    Insert first idea for saving data in file

--- app/src/main/java/com/example/randomizer/MainActivity.kt
+++ app/src/main/java/com/example/randomizer/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.randomizer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +16,35 @@ class MainActivity : AppCompatActivity() {
         val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
         val seekBar = findViewById<SeekBar>(R.id.seekBar)
 
-        rollButton.setOnClickListener {
+       rollButton.setOnClickListener {
             val rand = Random.nextInt(seekBar.progress) + 1
             resultsTextView.text = rand.toString()
+
+/*            //Create a new file that points to the root directory, with the given name:
+
+            //Create a new file that points to the root directory, with the given name:
+            val file = File(getExternalFilesDir(null), filenameExternal)
+
+            //This point and below is responsible for the write operation
+
+            //This point and below is responsible for the write operation
+            var outputStream: FileOutputStream? = null
+            try {
+                file.createNewFile()
+                //second argument of FileOutputStream constructor indicates whether
+                //to append or create new file if one exists
+                outputStream = FileOutputStream(file, true)
+                outputStream.write(textToWrite.getBytes())
+                outputStream.flush()
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+*/
         }
+
+
     }
+
+
 }
